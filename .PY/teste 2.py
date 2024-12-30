40d7ccae5b294ffc9da91c20293ad4982609aa33
Guilherme Castro <gui.castro.0001.1426@gmail.com>
2024-12-30 14:28:38 -0300

    teste

--- .PY/teste 2.py
+++
@@ -1,8 +0,0 @@
-nome = input("digite um nome")
-
-if nome == "guilherme":
-    print("o nome e normal")
-elif nome == "dario":
-    print("bunitão")
-else:               
-    print("nome nao é valido")
